e4ed4bb95b6bf034bf78a693cbf2c210ac0078c4
gozaing <steganographi@gmail.com>
2016-12-10 23:07:18 +0900

    具象クラスのConstructorにUser 引数を追加

--- strategy/sample1.php
+++ strategy/sample1.php
@@ -1,15 +1,37 @@
 <?php
 
+// 送信対象のUserクラス
+class User {
+    private $name;
+    private $mail;
+    private $tel;
+
+    // 仮に利用するpropertyを定義
+    public function __construct($name, $mail, $tel) {
+        $this->name = $name;
+        $this->mail = $mail;
+        $this->tel  = $tel;
+    }
+
+    // 仮に名前だけ出力するためにgetter設置
+    public function getName() {
+        return $this->name;
+    }
+
+}
+
 // 抽象クラス
 abstract class MessageStrategy {
+    protected $user;
     public abstract function send();
+    public abstract function __construct(User $user);
 }
 
 // コンテキストクラス
 class MessageContext {
     private $strategy;
 
-    public function __construct(MessageStrategy $message, $user) {
+    public function __construct(MessageStrategy $message) {
         $this->strategy = $message;
 
     }
@@ -19,35 +41,62 @@ class MessageContext {
     }
 }
 
-// 具象クラス
+// 具象クラス(Mail)
 class ConcreteMailMessage extends MessageStrategy {
-    public function __construct() {
-        // 前処理
+    public function __construct(User $user) {
+        $this->user = $user;
+        // mailに関する前処理
+        // ...
     }
 
     public function send() {
-        echo 'mail'.PHP_EOL;
+        echo 'mail->to('. $this->user->getName(). ')'. PHP_EOL;
     }
 }
+
+// 具象クラス(SMS)
 class ConcreteSMSMessage extends MessageStrategy {
-    public function __construct() {
-        // 前処理
+    public function __construct(User $user) {
+        $this->user = $user;
+        // smsに関する前処理
+        // ...
     }
 
     public function send() {
-        echo 'sms'.PHP_EOL;;
+        echo 'sms->to('. $this->user->getName(). ')'. PHP_EOL;
     }
 }
+
+// 具象クラス(PushNotification)
 class ConcretePushNotificationMessage extends MessageStrategy {
-    public function __construct() {
-        // 前処理
+    public function __construct(User $user) {
+        $this->user = $user;
+        // Push通知に関する前処理
+        // ...
     }
 
     public function send() {
-        echo 'push'.PHP_EOL;;
+        echo 'push->to('.$this->user->getName(). ')'. PHP_EOL;
     }
 }
 
-$strategy = new ConcreteMailMessage();
+// 送信対象のテストUserインスタンス
+$user = new User('Pさん', 'test@example.com', '090xxxxyyyy');
+
+// Mailが送信される
+$strategy = new ConcreteMailMessage($user);
+
+// $strategyにどのクラスを代入するかでMail / SMS / PushNotification とsend()の振る舞いを変える
+// SMSが送信される
+// $strategy = new ConcreteSMSMessage($user);
+
+// Push通知が送信される
+// $strategy = new ConcretePushNotificationMessage($user);
+
+// Contextクラスにstrategyクラスを注入
 $contextMessage = new MessageContext($strategy);
+
+// 注入されたオブジェクトのsend()メソッドをcallする
 $contextMessage->send();
+
+// mail->to(Pさん) と出力される
